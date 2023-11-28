99ee6703d3c5743b81819157b0cae2ec2b01e12a
Gustavo Camargo <gustavoscamargo@outlook.com>
2023-11-28 17:13:49 +0000

    Started user login and signup

--- db/schema.rb
+++ db/schema.rb
@@ -71,6 +71,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_142952) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
