ad25246a9f388689594a067f85fc76c59a3dab06
Martim Costa <martimcosta95@gmail.com>
2023-11-30 17:56:26 +0000

    Added links to the sidebar - NOT COMPLETE

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  get "dashboard", to: "pages#dashboard", as: :dashboard
+
   resources :songs, only: %i[index new create show]
 end
