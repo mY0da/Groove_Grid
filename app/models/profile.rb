1d893023742995c5b0b776db70b968be48f8305e
Gustavo_C <gustavoscamargo@outlook.com>
2023-11-30 15:57:00 +0000

    profiles

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_many :tags_profiles
   has_many :tags, through: :tags_profiles
+  validates :first_name, :last_name, :avatar
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "pages#home"
 
   resources :songs, only: [:index, :new, :create, :show]
+
+  resources :profiles, only: [:new,:create]
+
 end
