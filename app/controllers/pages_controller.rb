399099a268f3b05ca872e9182d9994d47be95580
Gustavo_C <gustavoscamargo@outlook.com>
2023-12-01 13:58:20 +0000

    arrows and end path implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    if current_user
+      redirect_to songs_path
+    end
   end
 
   def welcome_page
