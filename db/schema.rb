5a8ae09afef83a66b7d6b8f1f5cfab0fd61c9472
Martim Costa <martimcosta95@gmail.com>
2023-12-01 11:06:16 +0000

    fixed the user and profile relation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_30_162554) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,7 +77,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_162554) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
-
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
@@ -122,6 +120,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_162554) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,9 @@ User.destroy_all
 
 puts 'Creating users'
 
-first_user = User.create!(username: 'martimtonic', first_name: 'Martim', last_name: 'Costa', email: 'tony@merguez.com', password: 'test123')
+first_user = User.create!(username: 'martimtonic', email: 'tony@merguez.com', password: 'test123')
+
+profile = Profile.create!(user: first_user, first_name: 'Martim', last_name: 'Costa')
 
 puts 'Creating label'
 
