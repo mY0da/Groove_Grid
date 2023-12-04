58ad6c71dac81a52681a4685a3590be06b12d3e8
Gustavo_C <gustavoscamargo@outlook.com>
2023-12-04 14:35:19 +0000

    playlists first try

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
 
   resources :songs, only: %i[index new create show]
   resources :profiles, controller: 'profiles'
+  resources :playlists, only: %i[index new create show]
 end
