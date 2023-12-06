9475f9ff593a3a977a4b1cf049f5b3e46505e8dd
Gustavo_C <gustavoscamargo@outlook.com>
2023-12-06 21:01:07 +0000

    Add multiple songs to playlist

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,5 +1,5 @@
-import { Controller } from "@hotwired/stimulus"
-import TomSelect from 'tom-select'
+
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
