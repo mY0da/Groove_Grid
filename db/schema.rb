48ad3501ca52ac6473c69cf511e4a62238ee551a
Martim Costa <martimcosta95@gmail.com>
2023-12-07 12:49:11 +0000

    images on playlists and tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_162554) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_07_122954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_162554) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
+    t.integer "image_number"
     t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
