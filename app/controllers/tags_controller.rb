8e27c4ded64bbc020d5b09fab3d4d41d88b36ed3
Gustavo_C <gustavoscamargo@outlook.com>
2024-01-30 17:07:05 +0000

    Added button to create a playlist from the tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :set_tag, only: %i[show destroy]
+  before_action :set_tag, only: %i[show destroy download]
 
   def index
     # @tags = Tag.joins(:songs).where(songs: { user: current_user })
@@ -19,6 +19,24 @@ class TagsController < ApplicationController
   def destroy
     @tag.destroy
     redirect_to tags_path, status: :see_other
+  end
+
+  def download
+    zip_filename = "tag_#{@tag.id}_audio_files.zip"
+
+    temp_file = Tempfile.new(zip_filename)
+    Zip::File.open(temp_file.path, Zip::File::CREATE) do |zipfile|
+      @tag.songs.each do |song|
+        next unless song.audio_file.attached?
+
+        audio_file = song.audio_file
+        file_data = Net::HTTP.get_response(URI.parse(audio_file.url)).body
+        zipfile.get_output_stream(song.audio_file.filename.to_s) { |f| f.write file_data }
+      end
+    end
+
+    send_file temp_file.path, filename: zip_filename, type: 'application/zip', disposition: 'attachment'
+    temp_file.close
   end
 
   private
